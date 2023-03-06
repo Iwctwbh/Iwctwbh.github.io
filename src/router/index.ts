32d9df46926856244b16f277eb858cede3dfa8ce
夜夜三倍速 <iwctwbh@gmail.com>
2023-03-06 19:42:07 +0800

    Update HomePage

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,11 @@ import {createRouter, createWebHistory} from "vue-router";
 // 公共路由
 export const constantRoutes = [
     {
+        path: "/Tools/Home",
+        name: "首页 Home Page",
+        component: () => import("../views/Home.vue")
+    },
+    {
         // 非路由节点，重定向至首页
         path: "/:pathMatch(.*)*",
         redirect: "/Tools/Home"
